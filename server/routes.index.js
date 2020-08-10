203c7e7e8a2d568512f3b4db79570da750605cfb
decimo3 <ruanfaustinocamellodasilva@gmail.com>
2020-08-10 10:16:06 -0300

    FIX: add default method path (need test)

--- server/routes.index.js
+++ server/routes.index.js
@@ -1,3 +1,14 @@
 const routesUser = require('./routes.user')
 const routesPost = require('./routes.post')
-module.exports = [...routesUser, ...routesPost]
+const padrao = {
+    method: 'GET',
+    path: '/*',
+    options: {
+        auth: false,
+      },
+    handler: (request, h) => {
+        console.log("Usuário tentando acessar rota inexistente!")
+        h.response("Usuário tentando acessar rota inexistente!").code(404)
+    }
+}
+module.exports = [padrao, ...routesUser, ...routesPost]
