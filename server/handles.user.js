ece2e0166549f01ec27db812a89a747e68c4682c
decimo3 <ruanfaustinocamellodasilva@gmail.com>
2020-10-19 18:16:59 -0300

    DONE: Responsibility to trigger the generation of the Token was received as the user is authenticated

--- server/handles.user.js
+++ server/handles.user.js
@@ -2,17 +2,25 @@ const usuarios = require('./usuarios')
 const Token = require('./auth.JWT')
 
 async function logarUsuario(req, res) {
-  const user = JSON.parse(req.payload)
+  if (typeof(req.payload) == "object") {
+    var user = req.payload
+  } else {
+    var user = JSON.parse(req.payload)
+  }
   return await usuarios.logarUsuarios(user.email, user.senha)
-    .then((token) => {
-      let Token = JSON.stringify({ token })
-      console.log(Token)
-      return res.response(Token).code(200)
+    .then(async (user) => {
+      const { _id, nome, email, telefone } = user
+      try {
+        const token = await Token.generateToken({ _id, nome, email, telefone })
+        return res.response({ token }).code(200)
+      } catch (err) {
+        throw new Error(err)
+      }
     })
     .catch((err) => {
       // TODO: Gerenciar melhor o erro e passar claramente o erro lançado
       console.warn("Erro ao logar os usuário!", err)
-      return res.response(JSON.stringify({ err })).code(401)
+      return res.response().code(401)
     })
 }
 
