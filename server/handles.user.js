9e06261c9cd9d8e3981a7c22b6b5fff362f14758
decimo3 <ruanfaustinocamellodasilva@gmail.com>
2020-10-25 16:30:14 -0300

    FIX: Switch error by response 401 (unautorized)

--- server/handles.user.js
+++ server/handles.user.js
@@ -14,13 +14,13 @@ async function logarUsuario(req, res) {
         const token = await Token.generateToken({ _id, nome, email, telefone })
         return res.response({ token }).code(200)
       } catch (err) {
-        throw new Error(err)
+        return res.response("Erro ao gerar o Token, tente logar novamente").code(500)
       }
     })
     .catch((err) => {
       // TODO: Gerenciar melhor o erro e passar claramente o erro lançado
-      console.warn("Erro ao logar os usuário!", err)
-      return res.response().code(401)
+      console.warn("Erro ao logar os usuário!")
+      return res.response("Erro ao autenticar usuário! Verifique se o usuário e senha estão corretos!").code(401)
     })
 }
 
